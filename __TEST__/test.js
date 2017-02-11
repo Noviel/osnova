b0d015c2d43b7559917750dcf81af31cb8e232c1
snov <snov.contact@gmail.com>
2017-02-12 02:17:43 +0700

    cleaning package.json

--- __TEST__/test.js
+++ __TEST__/test.js
@@ -1,10 +1,12 @@
 // Created by snov on 10.02.2017.
 //
-// ABOUT THIS FILE
+// Some quick tests.
+// TODO: write NORMAL tests!
 //
 /////////////////////////////////////////////////////////////////
 
 const osnova = require('../build/index.js');
+const { defaults }  = require('osnova-lib');
 
 const server = osnova.Server({
   core: {
@@ -22,6 +24,8 @@ const server = osnova.Server({
 server.start((osnova) => {
   const app = osnova.express;
   app.get('*', (req, res) => {
-    res.send('hello');
+    const obj = { a: 1, b: 2, c: { cc: 10} };
+    const def = { a: { aa: 10}, b: 10, c: { cc: 20, cd: 50} };
+    res.send(defaults(obj,def));
   })
 });
